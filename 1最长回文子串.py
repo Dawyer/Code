0a7a85158b9df651cc4db873d33317b7237ff1cc
yp <492388374@qq.com>
2019-11-11 21:46:56 +0800

    最长回文_方法一

--- 1最长回文子串.py
+++ 1最长回文子串.py
@@ -1,1 +1,17 @@
-#最长回文子串
+# 最长回文子串
+class Solution(object):
+    def longestPalindrome(self,s):
+        maxlen = 0
+        answer = ''
+        for i in range(len(s)):
+            for j in range(i,len(s)):
+                temp = s[i:j+1]
+                if temp == temp[::-1] and len(temp)>maxlen:
+                    maxlen = len(temp)
+                    answer = temp
+        return answer
+
+if __name__ == "__main__":
+    s="babad"
+    sl=Solution()
+    print(sl.longestPalindrome(s))
