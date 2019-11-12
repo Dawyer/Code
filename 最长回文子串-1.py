a94459325e18f17d9fe01dd54c5f940c544e7eb5
yp <492388374@qq.com>
2019-11-12 21:10:45 +0800

    最长回文子串_方法二

--- 最长回文子串-1.py
+++ 最长回文子串-1.py
@@ -1,11 +1,19 @@
 # 最长回文子串
 class Solution(object):
     def longestPalindrome(self,s):
+        '''
+        :param s: str
+        :return: str
+        '''
         maxlen = 0
         answer = ''
+        #i遍历字符串s
         for i in range(len(s)):
+            #j遍历i之后的字符串
             for j in range(i,len(s)):
+                #取出i和j之间的字符串
                 temp = s[i:j+1]
+                #temp字符串与翻转后相等即为回文子串
                 if temp == temp[::-1] and len(temp)>maxlen:
                     maxlen = len(temp)
                     answer = temp
