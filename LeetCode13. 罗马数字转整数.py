4d31fb2514c725d8476c9154885004d628f8bb33
yp <492388374@qq.com>
2019-11-23 22:28:59 +0800

    LeetCode

--- LeetCode13. 罗马数字转整数.py
+++ LeetCode13. 罗马数字转整数.py
@@ -11,6 +11,17 @@
 # 给定一个罗马数字，将其转换成整数。输入确保在 1 到 3999 的范围内。
 
 def romanToInt(s:str) -> int:
-    sum=0
-    int
-    
+    sum = 0
+    luoma = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    teshu = {'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
+    for i in teshu.keys():
+        if i in s:
+            sum += teshu[i]
+            s = s.replace(i,'')
+    for j in luoma.keys():
+        if j in s:
+            sum += luoma[j] * s.count(j)
+            s = s.replace(j, '')
+    return sum
+
+print(romanToInt('DXCIII'))
