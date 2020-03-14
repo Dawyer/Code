ff7de7dbbfef2894d0f43f21a67b2e4fb79bc98b
yp <492388374@qq.com>
2020-03-14 11:53:36 +0800

    20200314-LeetCode

--- LeetCode300. 最长上升子序列.py
+++ LeetCode300. 最长上升子序列.py
@@ -16,14 +16,33 @@
 # 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 #动态规划
+# def lengthOfLIS(nums) -> int:
+#     if not nums:return 0
+#     dp = []
+#     for i in range(len(nums)):
+#         dp.append(1)
+#         for j in range(i):
+#             if nums[i] > nums[j]:
+#                 dp[i] = max(dp[i],dp[j]+1)
+#     return max(dp)
+
+#贪心+二分查找
 def lengthOfLIS(nums) -> int:
-    if not nums:return 0
-    dp = []
-    for i in range(len(nums)):
-        dp.append(1)
-        for j in range(i):
-            if nums[i] > nums[j]:
-                dp[i] = max(dp[i],dp[j]+1)
-    return max(dp)
+    d=[]
+    for n in nums:
+        if not d or n > d[-1]:
+            d.append(n)
+        else:
+            l,r=0,len(d)-1
+            loc=r
+            while l <= r:
+                mid=(l+r) // 2
+                if d[mid]>=n:
+                    loc=mid
+                    r=mid-1
+                else:
+                    l=mid+1
+            d[loc]=n
+    return len(d)
 
 print(lengthOfLIS([10,9,2,5,3,7,101,18]))
