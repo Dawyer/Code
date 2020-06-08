c46a2543ec8d109d1c24fd21821a3727a307c7ae
yp <492388374@qq.com>
2020-06-08 22:46:45 +0800

    20200608-LeetCode

--- problems/LeetCode990. 等式方程的可满足性.py
+++ problems/LeetCode990. 等式方程的可满足性.py
@@ -38,10 +38,37 @@
 
 
 class Solution:
-    def equationPossible(self, equations) -> bool:
+    class UnionFind:
+        def __init__(self):
+            self.parent = list(range(26))
+
+        def find(self, index):
+            if index == self.parent[index]:
+                return index
+            self.parent[index] = self.find(self.parent[index])
+            return self.parent[index]
+
+        def union(self, index1, index2):
+            self.parent[self.find(index1)] = self.find(index2)
+
+
+    def equationsPossible(self, equations) -> bool:
+        uf = Solution.UnionFind()
+        for st in equations:
+            if st[1] == '=':
+                index1 = ord(st[0]) - ord("a")
+                index2 = ord(st[3]) - ord("b")
+                uf.union(index1, index2)
+
+        for st in equations:
+            if st[1] == "!":
+                index1 = ord(st[0]) - ord("a")
+                index2 = ord(st[3]) - ord("a")
+                if uf.find(index1) == uf.find(index2):
+                    return False
         return True
 
 
 equation1 = ["a==b", "b!=a"]
 solution1 = Solution()
-print(solution1.equationPossible(equation1))
+print(solution1.equationsPossible(equation1))
